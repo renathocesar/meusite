32767a3dfe6c4e5f8308b656ffa0460fe17698c9
renathocesar <renatocds2005@gmail.com>
2025-05-08 20:01:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function saudacao(){
+    const mensagem = document.getElementById("mensagem")
+    mensagem.textContent="Obrigado por visitar a minha página! Essa é a primeira página que eu crio, atráves das aulas de Fundamentos de Desenvolvimento de Software!"
+}
